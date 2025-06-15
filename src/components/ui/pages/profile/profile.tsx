515deec94bc528af89eae39c01e78718f1ee0335
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2025-06-15 15:23:42 +0300

    Profile bugs fixes
    - Fixed no ui card for order in order list
    - profile password change removed (Checklist: При переходе на страницу данных профиля отображаются имя и email пользователя.)

--- src/components/ui/pages/profile/profile.tsx
+++ src/components/ui/pages/profile/profile.tsx
@@ -50,19 +50,6 @@ export const ProfileUI: FC<ProfileUIProps> = ({
             icon={'EditIcon'}
           />
         </div>
-        <div className='pb-6'>
-          <Input
-            type={'password'}
-            placeholder={'Пароль'}
-            onChange={handleInputChange}
-            value={formValue.password}
-            name={'password'}
-            error={false}
-            errorText={''}
-            size={'default'}
-            icon={'EditIcon'}
-          />
-        </div>
         {isFormChanged && (
           <div className={styles.button}>
             <Button
